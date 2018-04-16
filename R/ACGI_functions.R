2a7f4eaba281f49c48d84fe013dd0a65b34cd9f3
Dean Rollins <Dean.Rollins@utas.edu.au>
2018-04-16 14:30:09 +1000

    Making the calc_GDD_weighted work with different budb and leaf inputs

--- R/ACGI_functions.R
+++ R/ACGI_functions.R
@@ -542,14 +542,10 @@ calc_GDD <- function(T_max, T_min, T_base, T_upper=1000) {
 #'
 #' @examples
 #' calc_GDD_weighted(GDD_base_budb, GDD_base_leaf, GDD_base_10, params)
-calc_GDD_weighted <- function(GDD_base_budb, GDD_base_leaf, GDD_base_10, params) {
-
-  # constants
-  base_budb <- params[["base_budb"]]
-  base_leaf <- params[["base_leaf"]]
-  bracket_budb_leaf <- params[["bracket_budb_leaf"]]
-  bracket_leaf_pick <- params[["bracket_leaf_pick"]]
-  DMT_threshold_pctile <- params[["DMT_threshold_pctile"]]
+calc_GDD_weighted <- function(T_max, Tmin, T_budb=4, T_leaf=7, T_vera=10, T_upper=1000, GDD_budb_threshold=350, GDD_leaf_threshold=1000, GDD_harvest_threshold=1300){
+  GDD_base_budb <- calc_GDD(T_max, T_min, T_base = T_budb, T_upper = T_upper)
+  GDD_base_leaf <- calc_GDD(T_max, T_min, T_base = T_leaf, T_upper = T_upper)
+  GDD_base_vera <- calc_GDD(T_max, T_min, T_base = T_vera, T_upper = T_upper)
 
   # calculated each of the three periods p1, p2, p3 separately
   # and create output vector by combining those
@@ -560,7 +556,7 @@ calc_GDD_weighted <- function(GDD_base_budb, GDD_base_leaf, GDD_base_10, params)
   # if threshold is not crossed set p1 to entire vector length
 
   p1 <- cumsum(GDD_base_budb)
-  p1_end_ind <- which(p1 > bracket_budb_leaf)[1]
+  p1_end_ind <- which(p1 > GDD_budb_threshold)[1]
   if (is.na(p1_end_ind)) p1_end_ind <- length(GDD_base_budb)
   p1_end <- p1[p1_end_ind]
 
@@ -575,7 +571,7 @@ calc_GDD_weighted <- function(GDD_base_budb, GDD_base_leaf, GDD_base_10, params)
     # calculate cumsum from the next element after the end of the first period
     # use the p1_end value as the fixed value to add to the cumsum (which starts at zero)
     p2 <- p1_end + cumsum(GDD_base_leaf[(p1_end_ind + 1):length(GDD_base_leaf)])
-    p2_end_ind <- which(p2 > bracket_leaf_pick)[1]
+    p2_end_ind <- which(p2 > GDD_leaf_threshold)[1]
     if (is.na(p2_end_ind)) p2_end_ind <- length(GDD_base_leaf)
     p2_end <- p2[p2_end_ind]
 
@@ -583,7 +579,7 @@ calc_GDD_weighted <- function(GDD_base_budb, GDD_base_leaf, GDD_base_10, params)
     if (p2_end_ind == length(GDD_base_leaf)) {
       p3 <- c()
     } else {
-      p3 <- p2_end + cumsum(GDD_base_10[(p1_end_ind + p2_end_ind + 1):length(GDD_base_10)])
+      p3 <- p2_end + cumsum(GDD_base_vera[(p1_end_ind + p2_end_ind + 1):length(GDD_base_vera)])
     }
   }
 
@@ -596,8 +592,8 @@ calc_GDD_weighted <- function(GDD_base_budb, GDD_base_leaf, GDD_base_10, params)
   # because we are creating the GDD_weighted vector by manually joining vectors
   # we have to ensure it is the same length as corresponding time series
   # it may be created too long
-  if(length(GDD_weighted) > length(GDD_base_10)) {
-    GDD_weighted <- GDD_weighted[1:length(GDD_base_10)]
+  if(length(GDD_weighted) > length(GDD_base_vera)) {
+    GDD_weighted <- GDD_weighted[1:length(GDD_base_vera)]
   }
 
   return(GDD_weighted)
